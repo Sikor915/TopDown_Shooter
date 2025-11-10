09f80d337f123e49be22d4821ce031a67800beea
Kacper Sikorski <115071106+Sikor915@users.noreply.github.com>
2025-11-10 20:09:10 +0100

    Created simple main menu and added scenes that need to be created

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnStartButtonPressed()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DemoScene");
+    }
+    
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
