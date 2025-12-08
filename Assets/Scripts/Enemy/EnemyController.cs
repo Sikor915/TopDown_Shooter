d314ced1ddb7689d1f7900ce880f76df79903f89
Kacper Sikorski <115071106+Sikor915@users.noreply.github.com>
2025-12-08 20:49:34 +0100

    Enemies drop weapons now

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -12,6 +12,11 @@ public class EnemyController : MonoBehaviour, IEnemy
     [SerializeField] PlayerSO playerSO;
     [SerializeField] PlayerController playerController;
     [SerializeField] Weapon enemyWeapon;
+    public Weapon EnemyWeapon
+    {
+        get { return enemyWeapon; }
+        set { enemyWeapon = value; }
+    }
 
     [Header("Enemy Senses")]
     [SerializeField] float fieldOfViewAngle;
@@ -206,6 +211,17 @@ public class EnemyController : MonoBehaviour, IEnemy
             GameObject moneyGO = Instantiate(MoneyManager.Instance.MoneyPrefab, transform.position, Quaternion.identity);
             MoneyObject moneyObject = moneyGO.GetComponent<MoneyObject>();
             moneyObject.Amount = enemySO.MoneyDropAmount;
+        }
+        chance = Random.Range(0, 100);
+        if (chance < 20)
+        {
+            Debug.Log("Dropping weapon");
+            GameObject weaponDrop = Instantiate(enemyWeapon.gameObject, transform.position, Quaternion.identity);
+            weaponDrop.SetActive(true);
+            weaponDrop.GetComponent<Weapon>().DropWeaponPrepare();
+            BoxCollider2D weaponCollider = weaponDrop.AddComponent<BoxCollider2D>();
+            weaponCollider.size = new Vector2(1.0f, 1.0f);
+            weaponCollider.isTrigger = true;
         }
         EnemySpawner.Instance.RemoveEnemy(gameObject);
         OnEnemyKilled?.Invoke(enemySO.EnemyScore);
